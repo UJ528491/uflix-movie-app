abb248245326cb8719f35a36db40fe770eb62f3a
UJ <hyj0829@gmail.com>
2021-02-04 01:24:19 +0900

    add styled-components, navbar css

---
+++ src/Components/Header.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  align-items: center;
+  padding: 0 10px;
+  background-color: rgb(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  height: 50px;
+  width: 100%;
+  display: flex;
+`;
+const Item = styled.li`
+  margin: 0 5px;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  .active {
+    border-bottom: 3px solid #f39c12;
+    transition: border-bottom 0.5s ease-in-out;
+  }
+`;
+const SLInk = styled(NavLink)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  border-bottom: 3px solid transparent;
+`;
+
+export default () => (
+  <Header>
+    <List>
+      <Item>
+        <SLInk exact to="/">
+          Movies
+        </SLInk>
+      </Item>
+      <Item>
+        <SLInk to="/tv">TV</SLInk>
+      </Item>
+      <Item>
+        <SLInk to="/search">Search</SLInk>
+      </Item>
+    </List>
+  </Header>
+);
---
+++ src/Components/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+import Header from "Components/Header";
+
+export default () => (
+  <Router>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      {/* <Route path="/tv/popular" render={() => <h1>Popular</h1>} /> */}
+      <Route path="/search" exact component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
+);
